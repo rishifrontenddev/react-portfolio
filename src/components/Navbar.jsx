59f80503a146e7fa80405f98ac321fde7b5026ea
hrishit <hrishit4280@gmail.com>
2025-09-24 16:49:51 +0530

    Update portfolio changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
-import { FaTwitterSquare } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
@@ -10,10 +9,30 @@ const Navbar = () => {
         <h1 className="mx-2 w-10">Rishi.dev</h1>
       </div>
       <div className="flex m-8 items-center justify-center gap-4  text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaTwitterSquare />
-        <FaInstagram />
+        <a
+          href="https://in.linkedin.com/in/hrishit-prakash-7666a5328"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          <FaLinkedin />{" "}
+        </a>
+        <a
+          href="https://github.com/rishifrontenddev"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          <FaGithub />{" "}
+        </a>
+        <a
+          href="https://www.instagram.com/hrishitunscripted/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          <FaInstagram />
+        </a>
       </div>
     </nav>
   );
