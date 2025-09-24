a120b8f7024c5639c2cf620b8437791268b6080a
hrishit <hrishit4280@gmail.com>
2025-09-24 16:31:41 +0530

    responsivess

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -30,7 +30,7 @@ export default function HeroSection() {
               initial="hidden"
               animate="visible"
               className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500
-            bg-clip-text text-3xl tracking-tight text-transparent "
+            bg-clip-text text-3xl tracking-tight text-transparent mr-20"
             >
               Front End Developer
             </motion.span>
